15ba9dafbff6466b3cea77de908ca5f35e3aa0d3
Hu Yangjian <huyangjian@sjtu.edu.cn>
2023-03-02 23:31:59 +0800

    修改发现界面，添加头像和tag和文本域

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import {Avatar, Space, Tag, TextArea} from 'antd-mobile';
 class Discover extends React.Component {
     render() {
-        return (<div>发现</div>);
+        return (
+        <Space direction={"vertical"}>
+            <Space  direction='horizontal'>
+                <Avatar src=''/>
+                <Tag color={"primary"}>
+                    学习
+                </Tag>
+            </Space>
+            <Space>
+                <TextArea style={{border: 1}} placeholder='请输入内容' value={'摆烂了, \n别摆烂好吗'} autoSize={{ minRows: 3, maxRows: 10 }}/>
+            </Space>
+        </Space>
+        );
         //TODO: your code here
     }
 }
