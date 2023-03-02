b938edd7ce9c167b7c13ecec5eeb7a2dc9241268
influ3nza <influenza-lv3@sjtu.edu.cn>
2023-03-02 10:13:36 +0800

    influ3nza: Stats界面ver0.1----优化项目结构 电脑没电了。

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import {NavBar, CapsuleTabs} from "antd-mobile";
+import "./Stats.css"
+
 
 class Stats extends React.Component {
+    state = {
+        activekey: "cap1"
+    }
+
+    capRender= () => {
+        if (this.state.activekey === "cap1") return <div>fuck</div>
+        else return <div>you</div>
+    }
+    onCapChange= (activeKey) => {
+        this.setState({activekey: activeKey})
+    }
+
     render() {
-        return (<div>统计</div>);
+        return (<div>
+            <div className="navBarField">
+                <NavBar onBack={this.onClickBack} className="navBar">回顾</NavBar>
+            </div>
+            <div className="filterField">
+                <div className="filterModeBox">
+                    <CapsuleTabs onChange={this.onCapChange}>
+                        <CapsuleTabs.Tab title="Yes" key="cap1">
+
+                        </CapsuleTabs.Tab>
+                        <CapsuleTabs.Tab title="No" key="cap2">
+
+                        </CapsuleTabs.Tab>
+                    </CapsuleTabs>
+                    <div className="capRender">
+                        {this.capRender()}
+                    </div>
+                </div>
+            </div>
+        </div>);
         //TODO: your code here
     }
 }
