7131715eb5d94b032434e251eb0c808589074788
influ3nza <influenza-lv3@sjtu.edu.cn>
2023-03-02 21:38:02 +0800

    influ3nza:Stats Ver0.4----优化event展示，从数组中直接读取 Nice boat.

--- src/pages/Stats/Stats_EventList.js
+++ src/pages/Stats/Stats_EventList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Stats.css'
-import { RightOutline } from "antd-mobile-icons";
+import {RightOutline} from "antd-mobile-icons";
 
 const divstyle = {
     display: 'flex',
@@ -8,22 +8,29 @@ const divstyle = {
     alignItems: 'center',
     height: '150px',
     backgroundImage: 'linear-gradient(30deg, rgb(92,255,168), rgb(92,192,168) 25%, rgb(92,192,168) 75%, rgb(92,255,168))',
-    marginBottom: 20,
+    marginBottom: 150,
     marginRight: 20,
     width: '75%',
     borderRadius: '20px',
     boxShadow: '0 0 2px 3px #ddd',
-    font: 'bold 30px Microsoft-YaHei'
-}
+    font: 'bold 30px Microsoft-YaHei',
+}//change position
 
 const stats_eventList = (eventlist) => {
-    return(<div className="eventListField" align="right">
-        {eventlist.map(sgevent => (
-            <div className={"sgeventBox" + sgevent + " sgeventBox"}
-                 style={divstyle}>
-                Event {sgevent}
-                <div className="sgeventIcon">
-                    <RightOutline fontSize={50}/>
+    return (<div className="eventListField" align="right">
+        <div className="stats_timescroll"/>
+        {eventlist.map((sgevent, idx) => (
+            <div className="stats_sgEventField" key={idx}>
+                <div className="stats_dateBox">
+                    {eventlist[idx].date}
+                </div>
+                <div className="stats_sgEventIcon"/>
+                <div className={"sgEventBox" + eventlist[idx].key + " sgEventBox"}
+                     style={divstyle}>
+                    Event {eventlist[idx].key}
+                    <div className="stats_sgEventArrow">
+                        <RightOutline fontSize={50}/>
+                    </div>
                 </div>
             </div>
         ))}
