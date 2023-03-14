6a0bb41e1eb1f4c723a31624ea22c6ad6ffb856d
influ3nza <influenza-lv3@sjtu.edu.cn>
2023-03-14 19:45:59 +0800

    influ3nza: Stats ver1.0----重做filter----完成标签筛选以及数据传递 接下来是日期筛选

--- src/pages/Stats/Stats.js
+++ src/pages/Stats/Stats.js
@@ -15,11 +15,14 @@ const options = [{label: 'Label1', value: '1',}, {label: 'Label2', value: '2',},
 class Stats extends React.Component {
     state = {
         category: "event",
-        filterOpen: true
+        filterOpen: true,
+        filterTagStatus: [],
+        filterTimeStatus: []
     }
 
     backAddr = '/home'
     goAddr = '/stats/details'
+    filterTagBuf = []
 
     capRender = () => {
         if (this.state.category === "event")
@@ -41,11 +44,31 @@ class Stats extends React.Component {
         if (this.state.filterOpen) {
             this.filter.classList.add('stats_filterAdd');
             this.filter.classList.remove('stats_filterRemove');
-        }
-        else {
+        } else {
             this.filter.classList.add('stats_filterRemove');
             this.filter.classList.remove('stats_filterAdd');
         }
+    }
+
+    onTagSelectorChange = (arr, extend) => {
+        const items = extend.items;
+        const tags = [];
+
+        items.map((e) => {
+            tags.push({value: e.value});
+        })
+
+        this.filterTagText = items.length > 1 ? '多个' :
+            items.length ? items[0].value : '';
+        this.filterTagBuf = arr;
+
+        this.setState({filterTagStatus: tags});
+    }
+
+    onTagReset = () => {
+        this.filterTagText = '';
+        this.filterTagBuf = [];
+        this.setState({filterTagStatus: []});
     }
 
     render() {
@@ -53,23 +76,38 @@ class Stats extends React.Component {
             <div className="stats_absoluteField">
                 <HeaderBar backFunc={OnClickRoute.bind(this, this.backAddr)} title='回顾'/>
                 <div className="stats_filterField"
-                     ref={(e) => {this.filter = e}}>
+                     ref={(e) => {
+                         this.filter = e
+                     }}>
                     <div className="stats_filterModeBox">
                         <Tabs onChange={this.onCapChange}>
                             <Tabs.Tab title="Events" key="event">
                                 <div className="stats_sift"
-                                     style={{position:'relative'}}>
-                                    所有
+                                     style={{position: 'relative'}}>
+                                    {this.state.filterTagStatus.length ?
+                                        (this.state.filterTimeStatus.length ? '混合' : '标签: ' + this.filterTagText) :
+                                        (this.state.filterTimeStatus.length ? '时间: ' : '所有')}
                                     <div className="stats_siftArrow"
-                                         style={{position:'absolute', right:'10px', top:'0'}}
+                                         style={{position: 'absolute', right: '10px', top: '0'}}
                                          onClick={this.onFilterGrow}>
                                         <UndoOutline/>
                                     </div>
                                 </div>
-                                <Selector columns={3} options={options}
-                                          showCheckMark={false}
-                                          multiple={true}
-                                          className="stats_selectorBox"/>
+                                <div className="stats_filterTags">
+                                    <div className="stats_tagsTitle">标签</div>
+                                    <div className="stats_tagsReset"
+                                         onClick={this.onTagReset}>
+                                        <UndoOutline/>
+                                    </div>
+                                    <div className="stats_tagsSelector">
+                                        <Selector columns={2} options={options}
+                                                  showCheckMark={false}
+                                                  multiple={true}
+                                                  className="stats_selectorBox"
+                                                  value={this.filterTagBuf}
+                                                  onChange={this.onTagSelectorChange}/>
+                                    </div>
+                                </div>
                             </Tabs.Tab>
                             <Tabs.Tab title="Statistics" key="stats"/>
                         </Tabs>
