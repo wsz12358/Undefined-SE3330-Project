9a26d6d0ddd4a9cb4294d1c927cdae9e0f9d53d7
influ3nza <influenza-lv3@sjtu.edu.cn>
2023-03-20 11:18:53 +0800

    influ3nza: Stats ver1.3----data-friendly modifications to BMap

--- UIPrototype/src/utils/EventListDemo.js
+++ UIPrototype/src/utils/EventListDemo.js
@@ -1,5 +1,11 @@
-const eventListDemo = [{key: '1', date: 'Dec.12'}, {key: '2', date: 'Dec.11'}, {key: '3', date: 'Dec.6'},
-    {key: '4', date: 'Dec.1'}, {key: '5', date: 'Nov.20'}, {key: '6', date: 'Nov.10'}, {key: '7', date: 'Nov.1'},
-    {key: '8', date: 'Sept.9'}]
+const eventListDemo = [
+    {key: '1', date: 'Dec.12', lat: 121.44329, mul: 31.03201},
+    {key: '2', date: 'Dec.11', lat: 121.44429, mul: 31.03201},
+    {key: '3', date: 'Dec.6', lat: 121.44529, mul: 31.03201},
+    {key: '4', date: 'Dec.1', lat: 121.44629, mul: 31.03201},
+    {key: '5', date: 'Nov.20', lat: 121.44329, mul: 31.03301},
+    {key: '6', date: 'Nov.10', lat: 121.44329, mul: 31.03401},
+    {key: '7', date: 'Nov.1', lat: 121.44329, mul: 31.03501},
+    {key: '8', date: 'Sept.9', lat: 121.44329, mul: 31.03601}]
 
 export default eventListDemo
