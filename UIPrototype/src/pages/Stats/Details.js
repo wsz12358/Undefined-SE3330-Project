b8685f3a7bfdb15737d8c6a3a67a6979dbcd8f8f
Hu Yangjian <huyangjian@sjtu.edu.cn>
2023-04-05 18:10:14 +0800

    修改Details界面:增加几个功能界面，修改布局（简介美）

--- UIPrototype/src/pages/Stats/Details.js
+++ UIPrototype/src/pages/Stats/Details.js
@@ -3,10 +3,11 @@ import HeaderBar from "../../components/HeaderBar";
 import OnClickRoute from "../../utils/OnClickRoute";
 import './Details.css'
 
-import {Badge, Button, Collapse, Image, Tag} from "antd-mobile";
+import {Badge, Button, Collapse, Image, List, Tag} from "antd-mobile";
 import jntm from "../../assets/jntm.png"
-import {AddCircleOutline} from "antd-mobile-icons";
+import {AddCircleOutline, ClockCircleOutline, EnvironmentOutline, EyeOutline, UserOutline} from "antd-mobile-icons";
 import eventListDemo from "../../utils/EventListDemo";
+import {ListItem} from "antd-mobile/es/components/list/list-item";
 
 
 const allThoughts = ["全民制作人大家好，我是练习时长两年半的个人练习生蔡徐坤，喜欢唱、跳、rap、篮球，music",
@@ -49,26 +50,29 @@ class Details extends React.Component {
             </div>
 
             <div className="detail_eventField">
-                <div className='deTime'>
-                    {this.focusEvent.date} 2023 <br/>
+                <div className="deTitle">
                     This is event {this.focusEvent.key}.
                 </div>
-
-                <div className='deTag'>
-                    <Tag color='primary' className='chTag'>唱</Tag>
-                    <Tag color='blue' className='chTag'>跳</Tag>
-                    <Tag color='red' className='chTag'>rap</Tag>
-                    <Tag color='green' className='chTag'>篮球</Tag>
+                <div className='deTime'>
+                    {this.focusEvent.date} 2023
                 </div>
-                <Collapse defaultActiveKey={['1']}>
-                    <Collapse.Panel key='感想' title='感想'>
+
+                {/*<div className='deTag'>*/}
+                {/*    <Tag color='primary' className='chTag'>唱</Tag>*/}
+                {/*    <Tag color='blue' className='chTag'>跳</Tag>*/}
+                {/*    <Tag color='red' className='chTag'>rap</Tag>*/}
+                {/*    <Tag color='green' className='chTag'>篮球</Tag>*/}
+                {/*</div>*/}
+
+                <Collapse defaultActiveKey={['1']} className="myCollapse">
+                    <Collapse.Panel key='感想' title='感想' className="myCollapsePanel">
                         {
                             <div className='allThoughts'>
                                 {allThoughts.map(this.renderThoughts)}
                             </div>
                         }
                     </Collapse.Panel>
-                    <Collapse.Panel key='图片' title='图片'>
+                    <Collapse.Panel key='图片' title='图片' className="myCollapsePanel">
                         {
                             <div className='allPictures'>
                                 <div className='picture'>
@@ -87,10 +91,31 @@ class Details extends React.Component {
                             </div>
                         }
                     </Collapse.Panel>
+                    <Collapse.Panel key='tag' title='tag' className="myCollapsePanel">
+                        {
+                            <div className="allTags">
+                                <div className="deTag">
+                                    吃饭
+                                </div>
+                                <div className="deTag">
+                                    睡觉
+                                </div>
+                                <div className="deTag">
+                                    打篮球
+                                </div>
+
+                            </div>
+                        }
+                    </Collapse.Panel>
                 </Collapse>
-                <div className='deLocation'>
-                    地点：美国
-                </div>
+
+                <List>
+                    <ListItem prefix={<EnvironmentOutline />} onClick={()=>{}}>所在位置</ListItem>
+                    <ListItem prefix={<EyeOutline />} onClick={()=>{}}>谁可以看</ListItem>
+                    <ListItem prefix={<UserOutline />} onClick={()=>{}}>提醒谁看</ListItem>
+                    <ListItem prefix={<ClockCircleOutline />} onClick={()=>{}}>定时</ListItem>
+                </List>
+
             </div>
         </div>);
     }
